f8cad0fddbc556874c30b94669a24589305f2efc
zentauro <pedrogomezmartin1999@gmail.com>
2019-10-23 21:00:36 +0200

    p04 menus and some more vscode config

---
+++ p04/include/menus.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+inline static constexpr const char *const menu1[30] = {
+    "Objeto local", "Objeto dinamico", "Constructor copia",
+    "Operador asignacion", "Terminar"};
+
+inline static constexpr const char *const menu2[30] = {
+    "Con hora predeterminada",
+    "Introduciendo la hora",
+    "Introduciendo la hora y los minutos",
+    "Introduciendo la hora, los minutos y los segundos",
+    "Introduciendo la hora, los minutos, los segundos y el formato",
+    "Volver al menu principal",
+};
